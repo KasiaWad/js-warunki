671d7283206eccc23e09cc93624aef9a6e5bb103
kasiaw <kasiawkasiaw1@gmail.com>
2021-10-10 15:55:22 +0200

    js-warunki

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ let house = 12;
 if (house > 10) {
 console.log('Podana liczba jest większa od 10');
 }
-else if (house !> 10) {
+else if (house < 10) {
     console.log('Podana liczba nie jest większa od 10');
 }
